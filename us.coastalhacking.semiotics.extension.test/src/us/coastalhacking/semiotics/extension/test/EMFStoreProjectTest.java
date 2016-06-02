531b136b5bf8e716202f46c493b05e59c52e5538
jonpasski <jon@passki.us>
2016-06-02 15:22:54 -0700

    EMFStore: Understand Project.getModelElements
    * Added models to provider run requirements
    * Created simple test to add both assessment and http to root container
    
    Closes #5.
    
    Task-Url:
    https://github.com/CoastalHacking/semiotics-extension/issues/issues/issue/5

---
+++ us.coastalhacking.semiotics.extension.test/src/us/coastalhacking/semiotics/extension/test/EMFStoreProjectTest.java
@@ -0,0 +1,41 @@
+package us.coastalhacking.semiotics.extension.test;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.emfstore.client.ESLocalProject;
+import org.eclipse.emf.emfstore.client.ESWorkspace;
+import org.eclipse.emf.emfstore.client.ESWorkspaceProvider;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import io.opensemantics.semiotics.model.assessment.Assessment;
+import io.opensemantics.semiotics.model.assessment.AssessmentFactory;
+import io.opensemantics.semiotics.model.assessment.Http;
+
+public class EMFStoreProjectTest {
+	
+	private ESWorkspace workspace;
+	private ESLocalProject project;
+
+	@Before
+	public void setup() {
+		workspace = ESWorkspaceProvider.INSTANCE.getWorkspace();
+		project = workspace.createLocalProject("getmodelelementstest");
+		project.getModelElements().clear();
+	}
+	/*
+	 * Can multiple EObject types be added to Project.getModelElements()?
+	 * For example, can a bunch of Sink objects be added?
+	 */
+	@Test
+	public void testProjectGetModelElements() throws Exception {
+		final EList<EObject> root = project.getModelElements();
+		Assert.assertTrue(root.isEmpty());
+		final Assessment assessment = AssessmentFactory.eINSTANCE.createAssessment();		
+		root.add(assessment);
+		final Http http = AssessmentFactory.eINSTANCE.createHttp();
+		root.add(http);
+		Assert.assertEquals(2, root.size());
+	}
+}
